c0610de6eb2f0aff10a0f29665addd1763a7ab93
Kareem2002Shimes <kareem4shimas@gmail.com>
2023-05-29 07:59:42 +0300

    transaltion

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,6 +4,11 @@ module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ar"],
+    pages: {
+      "*": ["common"],
+    },
+    reloadOnPrerender: process.env.NODE_ENV === "development",
     localeDetection: false,
+    localePath: path.resolve("./public/locales"),
   },
 };

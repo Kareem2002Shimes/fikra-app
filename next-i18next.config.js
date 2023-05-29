bfadd87bc26e1badcf2511b63983e99103ec263e
Kareem2002Shimes <kareem4shimas@gmail.com>
2023-05-29 08:13:29 +0300

    fix

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,11 +4,6 @@ module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ar"],
-    pages: {
-      "*": ["common"],
-    },
-    reloadOnPrerender: process.env.NODE_ENV === "development",
-    localeDetection: false,
-    localePath: path.resolve("./public/locales"),
   },
+  localePath: path.resolve("./public/locales"),
 };
